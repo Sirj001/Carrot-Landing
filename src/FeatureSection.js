8a1eebab53206f8761ab36c8dc41da1050cea127
Sirj001 <145116017+Sirj001@users.noreply.github.com>
2024-09-29 23:51:06 +0100

    Update FeatureSection.js

--- src/FeatureSection.js
+++ src/FeatureSection.js
@@ -3,7 +3,7 @@ const FeatureSection = () => {
   return (
     <section className="grid grid-cols-3 sec-1 gap-6 p-10">
       <div className="text-blacke p-4">
-        <img className="img" src="./images/cup.jpg.jpeg" alt="img 1"></img>
+        <img className="img" src="./images/cup.jpg.jpeg" alt=""></img>
         <h2>Create sustained positive change</h2>
         <p className="text-xs">
           Help people see the impact of their choices and provide continuous
@@ -12,7 +12,7 @@ const FeatureSection = () => {
       </div>
 
       <div className="text-black p-4">
-        <img className="img" src="./images/shape.jpg.jpeg" alt="img 1"></img>
+        <img className="img" src="./images/shape.jpg.jpeg" alt=""></img>
         <h2>Target actions and improvements</h2>
         <p className="text-xs">
           Get started where you are today Incentivize sustainable actions at
@@ -21,7 +21,7 @@ const FeatureSection = () => {
       </div>
 
       <div className="text-black p-4">
-        <img className="img" src="./images/home.jpg.jpeg" alt="img 1"></img>
+        <img className="img" src="./images/home.jpg.jpeg" alt=""></img>
         <h2>Get started where you are today </h2>
         <p className="text-xs">
           Incentivize sustainable actions at scale using existing systems or our
@@ -30,7 +30,7 @@ const FeatureSection = () => {
       </div>
 
       <div className="text-black p-4">
-        <img className="img" src="./images/double.jpg.jpeg" alt="img 1"></img>
+        <img className="img" src="./images/double.jpg.jpeg" alt=""></img>
         <h2>Break down data silos</h2>
         <p className="text-xs">
           Incentivize sustainable actions at scale using existing systems or our
@@ -39,7 +39,7 @@ const FeatureSection = () => {
       </div>
 
       <div className="text-black p-4">
-        <img className="img" src="./images/hot.jpg.jpeg" alt="img 1"></img>
+        <img className="img" src="./images/hot.jpg.jpeg" alt=""></img>
         <h2>Go from waste to resources</h2>
         <p className="text-xs">
           Empower users to take part in the circular economy by finding the

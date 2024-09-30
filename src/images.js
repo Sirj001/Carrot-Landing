cfd4a7e3c9b9bae85a443497053e06c0a9850a5f
Sirj001 <145116017+Sirj001@users.noreply.github.com>
2024-09-30 19:17:50 +0100

    Update images.js

--- src/images.js
+++ src/images.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const images = () => {
   return (
-    <section className="p-10 grid grid-cols-2 pic-01 border gap-4">
+    <section className="p-10 grid md:grid-cols-2 pic-01 border gap-4">
       <div>
         <p className="text-xs">Real estate</p>
         <h1 className="text-3xl">
@@ -13,9 +13,6 @@ const images = () => {
           sorting efforts. Find the optimal receiver for used resources and open
           the door to new revenue streams.
         </h2>
-        <button className="bg-black text-white py-1 px-2 rounded">
-          Discover how
-        </button>
       </div>
       <div>
         <img
@@ -23,6 +20,9 @@ const images = () => {
           alt="Waste Management"
           className="w-1/2 h-auto"
         />
+        <button className="bg-black text-white font-medium m py-1 px-2 rounded">
+          Discover how
+        </button>
       </div>
 
       <div className="mb-9">
@@ -34,17 +34,17 @@ const images = () => {
           Get a full overview of infrastructure, resources, and user data, and
           use insights to reward and motivate sustainable behavior.
         </h2>
-        <button className="bg-black text-white py-1 px-2 rounded">
-          Discover how
-        </button>
       </div>
 
-      <div className="mb-9">
+      <div>
         <img
           src="./images/engineer.jpg.jpeg"
           alt="Waste Management"
           className="w-1/2 h-auto"
         />
+         <button className="bg-black text-white font-medium mt-2 py-1 px-2 rounded">
+          Discover how
+        </button>
       </div>
     </section>
   );
